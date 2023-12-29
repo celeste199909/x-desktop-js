9544e407e4aa8c509992c020c99b928d5b408f96
hi <oncwnuMdBWO26h0AJ4vJFH1UI0Hc@git.weixin.qq.com>
2023-12-30 00:09:21 +0800

    完成多屏切换功能

--- index/renderer.js
+++ index/renderer.js
@@ -61,13 +61,25 @@ class Icon {
     // 单个icon的点击事件
     iconElement.click((e) => {
       // console.log("icon clicked", e);
+      // 阻止事件冒泡
+      e.stopPropagation();
       utools.shellOpenPath(this.realPath);
       window.hideDesk();
     });
+    // 单个icon的双击事件
+    // iconElement.dblclick((e) => {
+    //   // console.log("icon dblclicked", e);
+    //   // 阻止事件冒泡
+    //   e.stopPropagation();
+    //   utools.shellOpenPath(this.realPath);
+    //   window.hideDesk();
+    // });
     // 把搜索索引的第一个关键字作为类名
     iconElement.addClass(this.searchKeywords[0]);
     // 单个icon的图片
     const iconImageElement = $("<img>").addClass("icon-image");
+    // 禁止拖动图片
+    iconImageElement.attr("draggable", false);
     iconImageElement.attr("src", this.iconImage);
     // 单个icon的名称
     const iconNameElement = $("<span></span>").addClass("icon-name");
@@ -80,11 +92,11 @@ class Icon {
 }
 
 class DesktopView {
-  constructor(setting, rawIcons) {
+  constructor(layout, setting, rawIcons) {
+    this.layout = layout;
     this.setting = setting;
     this.icons = rawIcons;
     // this.theme = setting.theme;
-    this.iconsInstance = [];
     this._searchTargets = []; // 搜索目标的数组
     this.selectedTargetIndex = 0; // 选中的搜索目标的索引
     // 搜索文本
@@ -93,6 +105,10 @@ class DesktopView {
     this._onTypingSearch = false;
     // 输入倒计时
     this._typingTimer = null;
+    // 页面数据
+    this.pages = []; // page [ { cellId: "", row: 1, coloum: 1, icon: Icon }, ... ]
+    // 当前页面索引
+    this.currentPageIndex = 0;
   }
 
   get searchText() {
@@ -140,23 +156,104 @@ class DesktopView {
 
   // 初始化
   init() {
+    // 设置布局
+    this.setLayout();
     // 应用设置
     this.applySettings();
     // 处理图标信息
     this.handleDesktopIcons();
     // 渲染桌面图标
     this.renderDesktopIcons();
-    // 监听按下键盘时搜索图标事件
+    // 搜索：监听按下键盘时搜索图标事件
     this.searchDesktopIconsListener();
-    // 切换主题
-    this.themeListener();
-    // 添加 keyup 监听事件
+    // 元素：添加点击事件监听
+    // 用于 1 切换主题 2 打开/关闭侧边栏
+    this.elementClickListener();
+    // 键盘：添加 keyup 监听事件
     // 空格：打开选中目标
     // 上下左右：切换选中目标
     // 回车：打开选中目标
     this.keyupListener();
+    // 拖动页面 / 鼠标滚轮 切换页面
+    // this.dragPageListener();
+    this.mousewheelListener();
   }
 
+  // 设置布局
+  setLayout() {
+    console.log("this.layout", this.layout);
+    console.log("this.icons", this.icons);
+    // 计算页面数量
+    const pagesLen = Math.ceil(
+      this.icons.length / (this.layout.row * this.layout.column)
+    );
+    // 创建页面数据
+    // 填充数据 并创建容器 创建页面
+    for (let pageIndex = 0; pageIndex < pagesLen; pageIndex++) {
+      const page = [];
+      const pageElement = $("<div></div>")
+        .addClass("page")
+        .attr("id", `page-${pageIndex + 1}`)
+        .appendTo("#pages");
+      // 设置页面padding-bottom
+      pageElement.css({
+        "padding-bottom": `${this.layout.paddingY * this.layout.height *  1.2}px`,
+      });
+      const appsCellWrapper = $("<div></div>")
+        .addClass("apps-wrapper")
+        .appendTo(pageElement);
+      // // 设置grid布局
+      appsCellWrapper.css({
+        width: `${this.layout.column * this.layout.cellLength}px`,
+        height: `${this.layout.row * this.layout.cellLength}px`,
+        "grid-template-columns": `repeat(${this.layout.column}, ${this.layout.cellLength}px)`,
+        "grid-template-rows": `repeat(${this.layout.row}, ${this.layout.cellLength}px)`,
+        // 间隔
+        "column-gap": `${this.layout.cellLength * 0.1}px`,
+      });
+      // 创建单元格
+      for (let i = 0; i < this.layout.row * this.layout.column; i++) {
+        const cell = {
+          cellId: `${pageIndex + 1}-${i + 1}`,
+          row: Math.floor(i / this.layout.column) + 1,
+          coloum: (i % this.layout.column) + 1,
+          icon: null,
+        };
+        page.push(cell);
+        const cellElement = $("<div></div>")
+          .addClass("cell")
+          .attr("id", cell.cellId)
+          .appendTo(pageElement);
+        // 设置宽高和padding
+        cellElement.css({
+          width: `${this.layout.cellLength}px`,
+          height: `${this.layout.cellLength}px`,
+        });
+        appsCellWrapper.append(cellElement);
+      }
+      this.pages.push(page);
+    }
+    console.log("this.pages:", this.pages);
+    // 创建页面切换指示器
+    for (let i = 0; i < pagesLen; i++) {
+      const indicate = $("<div></div>")
+        .addClass("page-indicate")
+        .appendTo("#pages-indicate");
+      // 设置点击事件
+      indicate.click(() => {
+        this.currentPageIndex = i;
+        this.moveToPage(this.currentPageIndex);
+        // 移除所有的指示器类名
+        $(".current-page-indicate").removeClass("current-page-indicate");
+        // 设置当前页面指示器
+        indicate.addClass("current-page-indicate");
+      });
+      // 设置当前页面指示器
+      if (i === this.currentPageIndex) {
+        indicate.addClass("current-page-indicate");
+      }
+    }
+  }
   // 应用设置
   applySettings() {
     // 设置主题
@@ -225,21 +322,19 @@ class DesktopView {
   }
   // 渲染桌面图标信息
   renderDesktopIcons() {
-    // console.log("Desktop Icons:", desktopIcons);
-    // 全部icon的容器
-    const iconsWrapper = $("<div></div>").addClass("icons-wrapper");
-
-    // 遍历icons数组，渲染图标
-    this.icons.forEach((icon) => {
-      // 创建icon实例
-      const iconInstance = new Icon(icon);
-      this.iconsInstance.push(iconInstance);
-      // 获取icon元素
-      const iconElement = iconInstance.getIconElement();
-      iconsWrapper.append(iconElement);
+    const icons = [...this.icons];
+    // 遍历pages数组，渲染图标
+    this.pages.forEach((page) => {
+      // 遍历icons数组，渲染图标
+      page.forEach((cell) => {
+        // 遍历icons数组，渲染图标
+        if (icons.length > 0) {
+          const iconInstance = new Icon(icons.shift());
+          cell.icon = iconInstance;
+          $(`#${cell.cellId}`).append(iconInstance.getIconElement());
+        }
+      });
     });
-    // console.log("this.iconsInstance:", this.iconsInstance);
-    $("#app").append(iconsWrapper);
   }
   // -------------- 搜索相关 -------------------
   // 取消所有的搜索目标
@@ -298,25 +393,34 @@ class DesktopView {
         this.searchText += event.key;
       }
 
-      // 遍历icons对象，匹配搜索关键字
-      this.iconsInstance.forEach((icon) => {
-        // 对搜索关键字列表进行遍历 如果搜索关键字中的任意一个单词匹配成功，则匹配成功
-        for (let i = 0; i < icon.searchKeywords.length; i++) {
-          if (icon.searchKeywords[i].includes(this.searchText.toLowerCase())) {
-            icon.searchTarget = true; // 匹配成功
-            this.searchTargets = [...this.searchTargets, icon]; // 添加到搜索目标数组中
-            break;
-          } else {
-            icon.searchTarget = false; // 未匹配成功
+      // 搜索目标
+      this.pages.forEach((page) => {
+        page.forEach((cell) => {
+          if (cell.icon) {
+            const icon = cell.icon;
+            for (let i = 0; i < icon.searchKeywords.length; i++) {
+              if (
+                icon.searchKeywords[i].includes(this.searchText.toLowerCase())
+              ) {
+                icon.searchTarget = true; // 匹配成功
+                this.searchTargets = [...this.searchTargets, icon]; // 添加到搜索目标数组中
+                break;
+              } else {
+                icon.searchTarget = false; // 未匹配成功
+              }
+            }
           }
-        }
+        });
       });
-      this.searchTargets[this.selectedTargetIndex].selectedTarget = true; // 选中搜索目标
 
-      // 设置定时器，在三秒后退出搜索状态
+      // 如果有搜索目标，则根据 selectedTargetIndex 选中
+      if (this.searchTargets.length > 0) {
+        this.searchTargets[this.selectedTargetIndex].selectedTarget = true; // 选中搜索目标
+      }
+
       this._typingTimer = setTimeout(() => {
         this.onTypingSearch = false;
-      }, 5000);
+      }, 9000);
     });
   }
   // 添加空格监听事件:打开选中目标 和 切换选中目标
@@ -337,8 +441,11 @@ class DesktopView {
         }
         return;
       }
-      // 判断是否按下的是从37到40上下左右键，上键38 下键40 左37 右39
-      if (event.keyCode >= 37 && event.keyCode <= 40) {
+      // 判断是否按下的是从上下键，上键38 下键40 并且是在搜索状态
+      if (
+        event.keyCode === 38 ||
+        (event.keyCode === 40 && this.onTypingSearch)
+      ) {
         // 判断searchTargets数组是否为空
         if (!this.searchTargets.length) {
           return;
@@ -347,16 +454,16 @@ class DesktopView {
         clearTimeout(this._typingTimer);
         // 取消当前选中目标
         this.searchTargets[this.selectedTargetIndex].selectedTarget = false;
-        // 如果按下的是上键或左键
-        if (event.keyCode === 38 || event.keyCode === 37) {
+        // 如果按下的是上键
+        if (event.keyCode === 38) {
           // 如果当前选中目标索引大于0，则选中上一个
           this.selectedTargetIndex--;
           if (this.selectedTargetIndex < 0) {
             this.selectedTargetIndex = this.searchTargets.length - 1;
           }
         }
-        // 如果按下的是下键或右键
-        if (event.keyCode === 40 || event.keyCode === 39) {
+        // 如果按下的是下键
+        if (event.keyCode === 40) {
           // 如果当前选中目标索引小于搜索目标数组的长度减1，则选中下一个
           this.selectedTargetIndex++;
           if (this.selectedTargetIndex > this.searchTargets.length - 1) {
@@ -374,11 +481,39 @@ class DesktopView {
         window.hideDesk();
         return;
       }
+
+      // 按下左右键切换页面
+      if (event.keyCode === 37 || event.keyCode === 39) {
+        // 如果按下的是左键
+        if (event.keyCode === 37) {
+          this.currentPageIndex--;
+          if (this.currentPageIndex < 0) {
+            this.currentPageIndex = 0;
+          }
+        }
+        // 如果按下的是右键
+        if (event.keyCode === 39) {
+          this.currentPageIndex++;
+          if (this.currentPageIndex > this.pages.length - 1) {
+            this.currentPageIndex = this.pages.length - 1;
+          }
+        }
+        // 移动页面
+        this.moveToPage(this.currentPageIndex);
+        // 移除所有的指示器类名
+        $(".current-page-indicate").removeClass("current-page-indicate");
+        // 设置当前页面指示器
+        $(`.page-indicate:eq(${this.currentPageIndex})`).addClass(
+          "current-page-indicate"
+        );
+        return;
+      }
     });
   }
   // ----------------------------------------------
-  // 监听主题切换按钮
-  themeListener() {
+  // 监听主题切换按钮/侧边栏按钮
+  elementClickListener() {
+    // 侧边栏的设置部分
     // 监听主题切换按钮
     $("#light-theme").click(() => {
       console.log("light-theme clicked");
@@ -403,6 +538,34 @@ class DesktopView {
       utools.db.put(this.setting);
       this.switchTheme();
     });
+
+    // sidebar 监听事件 打开/关闭侧边栏
+    $("#sidebar-btn").click(() => {
+      $("#sidebar").toggleClass("active");
+      $("#sidebar-btn").toggleClass("active");
+    });
+    // 关闭侧边栏
+    $("#close-sidebar-btn").click(() => {
+      $("#sidebar").removeClass("active");
+      $("#sidebar-btn").removeClass("active");
+    });
+
+    // 给左按钮添加点击事件
+    $("#prev-btn").click(() => {
+      this.currentPageIndex--;
+      if (this.currentPageIndex < 0) {
+        this.currentPageIndex = 0;
+      }
+      this.moveToPage(this.currentPageIndex);
+    });
+    // 给右按钮添加点击事件
+    $("#next-btn").click(() => {
+      this.currentPageIndex++;
+      if (this.currentPageIndex > this.pages.length - 1) {
+        this.currentPageIndex = this.pages.length - 1;
+      }
+      this.moveToPage(this.currentPageIndex);
+    });
   }
   // 切换主题
   switchTheme() {
@@ -424,15 +587,120 @@ class DesktopView {
       }
     }
   }
+  // 移到某一页
+  moveToPage(pageIndex, extraDistance = 0, duration = 300) {
+    if(pageIndex < 0){
+      this.currentPageIndex = 0;
+      return;
+    }
+    if(pageIndex > this.pages.length - 1){
+      this.currentPageIndex = this.pages.length - 1;
+      return;
+    }
+    // 修改当前页面索引
+    this.currentPageIndex = pageIndex;
+    // 移动页面 播放一次动画
+    $("#pages").css({
+      transform: `translateX(${
+        this.layout.width * -pageIndex + extraDistance
+      }px)`,
+      transition: `transform ${duration}ms ease-in-out`,
+    });
+    // 移除所有的指示器类名
+    $(".current-page-indicate").removeClass("current-page-indicate");
+    // 设置当前页面指示器
+    $(`.page-indicate:eq(${pageIndex})`).addClass("current-page-indicate");
+  }
+  // 通过拖动page切换页面
+  dragPageListener() {
+    // 监听 #app 鼠标按下事件
+    $("#app").mousedown((e) => {
+      // 如果不是在桌面上按下鼠标，则返回
+      console.log(e);
+      // if (
+      //   e.target.className !== "page" &&
+      //   e.target.className !== "cell" &&
+      //   e.target.className !== "apps-wrapper"
+      // ) {
+      //   return;
+      // }
+      console.log("page mousedown");
+      // 获取鼠标按下时的坐标
+      const startX = e.clientX;
+      // 监听 page 鼠标移动事件
+      $("#app").mousemove((e) => {
+        console.log("page mousemove");
+        // 获取鼠标移动时的坐标
+        const moveX = e.clientX;
+        // 计算移动的距离
+        const distanceX = moveX - startX;
+        // 移动页面
+        this.moveToPage(this.currentPageIndex, distanceX, 0);
+      });
+
+      // 监听 #app 鼠标抬起事件
+      $("#app").mouseup((e) => {
+        console.log("page mouseup");
+        // 获取鼠标抬起时的坐标
+        const endX = e.clientX;
+        // 计算移动的距离
+        const distanceX = endX - startX;
+        // 鼠标向左拖动 移动的距离大于页面宽度的1 / 3，则切换到下一页
+        if (
+          distanceX < 0 &&
+          Math.abs(distanceX) > this.layout.width / 4 &&
+          this.currentPageIndex < this.pages.length - 1
+        ) {
+          this.currentPageIndex++;
+          if (this.currentPageIndex > this.pages.length - 1) {
+            this.currentPageIndex = this.pages.length - 1;
+          }
+        }
+        // 如果移动的距离小于页面宽度的一半，则切换到上一页
+        if (
+          distanceX > 0 &&
+          Math.abs(distanceX) > this.layout.width / 4 &&
+          this.currentPageIndex > 0
+        ) {
+          this.currentPageIndex--;
+          if (this.currentPageIndex < 0) {
+            this.currentPageIndex = 0;
+          }
+        }
+        // 移动页面
+        this.moveToPage(this.currentPageIndex);
+        // 移除监听事件
+        $("#app").off("mousemove");
+        $("#app").off("mouseup");
+      });
+    });
+  }
+  // 通过鼠标滚轮切换页面
+  mousewheelListener() {
+    const app = document.querySelector("#app");
+    app.addEventListener("wheel", (e) => {
+      if(e.deltaY < 0){
+        this.currentPageIndex--;
+        this.moveToPage(this.currentPageIndex);
+      }
+      if(e.deltaY > 0){
+        this.currentPageIndex++;
+        this.moveToPage(this.currentPageIndex);
+      }
+    })
+  }
+    
 }
 
 $(document).ready(function () {
+  // 获取桌面布局信息
+  const deskLayout = getDeskLayout();
   // 获取本地设置
   const localSetting = getLocalSettings();
   // 获取图标信息
   window.getDesktopIcons(function (rawIcons) {
     // 初始化桌面视图
-    const desktopView = new DesktopView(localSetting, rawIcons);
+    const desktopView = new DesktopView(deskLayout, localSetting, rawIcons);
     desktopView.init();
   });
 });
@@ -457,4 +725,26 @@ function getLocalSettings() {
     settings = res;
   }
   return settings;
+}
+
+// 获取桌面布局信息
+function getDeskLayout() {
+  let deskLayout = {
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
+    paddingX: 0.1,
+    paddingY: 0.08,
+    cellLength: 140,
+    row: 0,
+    column: 0,
+  };
+
+  deskLayout.column = Math.floor(
+    (deskLayout.width * (1 - deskLayout.paddingX * 2)) / deskLayout.cellLength
+  );
+  deskLayout.row = Math.floor(
+    (deskLayout.height * (1 - deskLayout.paddingY * 2)) / deskLayout.cellLength
+  );
+
+  return deskLayout;
 }
